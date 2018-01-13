77965fa84e15b9df5296bcf78fc747a04d576deb
VicVenn <victor.vennerstroem@gmail.com>
2018-01-14 00:34:24 +0100

    Uppladdning utav alla filer

---
+++ Source/scroll.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(document).ready(function(){
+			$('a[href^="#"]').on('click',function (e) {
+				e.preventDefault();
+				
+				var target = this.hash;
+				var $target = $(target);
+				
+				$('html, body').animate({
+					'scrollTop': $target.offset().top
+				}, 1500, 'swing',function () {
+					window.location.hash = target;
+				});
+			});
+		});
